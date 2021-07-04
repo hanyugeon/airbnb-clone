760110594e7fc5892212fbda9232f071adbfb1fa
hanyugeon <hanyugeon@naver.com>
2021-07-04 18:52:08 +0900

    #11.2 make_pages_nav_using_a_href_tag

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,22 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 def all_rooms(request): # 04 core_urls.py에 all_rooms입력
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", context={
-        "potato": all_rooms # potato에 주목, templets_home.html 과 비교
-    })
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request, 
+        "rooms/home.html",
+        {
+            "potato": all_rooms, # potato에 주목, templets_home.html 과 비교
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1)
+        }
+    )
