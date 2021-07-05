9662f845364224588dbe6f56234085ad7067e4db
hanyugeon <hanyugeon@naver.com>
2021-07-05 23:59:00 +0900

    #11.4 using_paginator_and_recap

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,11 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 def all_rooms(request): # 04 core_urls.py에 all_rooms입력
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request, 
-        "rooms/home.html",
-        {
-            "potato": all_rooms, # potato에 주목, templets_home.html 과 비교
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1)
-        }
-    )
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", {"rooms": rooms})
