d762324307ca91e83274c3c4aed0f228f3cf0df8
hanyugeon <hanyugeon@naver.com>
2021-07-06 18:24:05 +0900

    #11.5 get_page_vs_page

--- rooms/views.py
+++ rooms/views.py
@@ -4,8 +4,8 @@ from django.core.paginator import Paginator
 from . import models
 
 def all_rooms(request): # 04 core_urls.py에 all_rooms입력
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)  # "page" / "page", 1
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    rooms = paginator.page(int(page))    # get_page(page) / page(int(page))
+    return render(request, "rooms/home.html", {"page": rooms})
