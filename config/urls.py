7a863e5c606875ecbdd6fa511d941b7c6df7634b
hanyugeon <hanyugeon@naver.com>
2021-07-12 01:08:42 +0900

    #12.0 urls_and_arguments

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [path("", include("core.urls", namespace="core")), path("admin/", admin.site.urls), ] # 02
+urlpatterns = [
+    path("", include("core.urls", namespace="core")), 
+    path("rooms/", include("rooms.urls", namespace="rooms")),   # rooms/urls.py
+    path("admin/", admin.site.urls), 
+    ] # 02
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
